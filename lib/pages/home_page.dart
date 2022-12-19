d4d83c8a91857889ca291eb0fc51fe13ce72b7e8
anibalmunoz <munoz4hernandez@gmail.com>
2022-12-19 11:22:05 -0600

    adicion

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_reader/pages/direcciones_page.dart';
 import 'package:qr_reader/pages/maps_history.dart';
+import 'package:qr_reader/providers/db_provider.dart';
 import 'package:qr_reader/providers/ui_provider.dart';
 import 'package:qr_reader/widgets/custom_navigator_bar.dart';
 
@@ -37,6 +38,7 @@ class _HomePageBody extends StatelessWidget {
   Widget build(BuildContext context) {
     final uiProvider = Provider.of<UiProvider>(context);
     final currentIndex = uiProvider.selectedMenuOpt;
+    DBProvider.db.database;
     switch (currentIndex) {
       case 0:
         return const MapsPage();
